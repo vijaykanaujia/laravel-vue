153879ff20efe292a62b3c4a577563c5650051fb
vijay kanaujia <vijaykanaujia3@gmail.com>
2022-05-12 20:22:18 +0530

    vue tutorail added

---
+++ resources/js/Pages/Vue3Tutorial/parent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>i am parent</div>
+    <child :name="name" :method="getData"></child>
+</template>
+
+<script>
+import ChildComponent from '@/Pages/Vue3Tutorial/child.vue';
+export default({
+    data(){
+        return {
+            name : 'Vijay'
+        }
+    },
+    setup() {
+        
+    },
+    components : {
+        'child' : ChildComponent
+    },
+    methods : {
+        getData(){
+            debugger;
+        }
+    }
+})
+</script>
